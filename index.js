cd39296f15841ec2cf229462e8fdc44d17323c1c
nadiaBatyk <nadia.batyk@gmail.com>
2023-09-23 12:14:35 +0930

    yargs wip

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
 import { readFile } from "fs/promises";
 import puppeteer from "puppeteer";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
+const argv = yargs(hideBin(process.argv))
+  .option("query", {
+    alias: ["q", "query"],
+    describe: "The job title / job query to perform",
+    type: "string",
+  })
+  .option("days", {
+    alias: ["d", "days"],
+    describe: "Days to perform the search",
+    default: 14,
+    type: "number",
+  })
+  .option("sort", {
+    alias: ["s", "sort"],
+    describe: "Sort the search result by date",
+    type: "boolean",
+  })
+  .option("keywords", {
+    alias: ["k", "keywords"],
+    describe: "Keywords to filter the results",
+    type: "array",
+    coerce: (k) => new RegExp(k.join("|"), "gi"),
+  })
+  .demandOption(
+    ["query", "keywords"],
+    "Please specify the query and at least 1 keyword"
+  )
+  .help()
+  .parse();
+console.log(argv);
 (async () => {
+  const query = argv.query?.split(" ").join("+");
+  const days = argv.days;
+  const sort = argv.sort;
+  const regexKeyWords = argv.keywords;
+  const url = `https://au.indeed.com/jobs?q=${query}&sort=${
+    sort ? "date" : ""
+  }&fromage=${days}`;
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  const query = process.env.QUERY?.split(" ").join("+") || "software+engineer";
-  const days = process.env.DAYS || "14";
-  const filtrarDias = process.env.FILTRARDIAS || false;
-  const keywords = [];
-  const regexKeyWords = new RegExp(keywords.join("|"), "gi");
-  const url = `https://au.indeed.com/jobs?q=${query}&sort=date ${
-    filtrarDias && `&fromage=${days}`
-  }`;
 
   await page.goto(url);
 
-  await page.waitForSelector("div.jobsearch-SerpMainContent");
-
+  await page.waitForSelector("div#mosaic-jobResults");
+  // console.log(regexKeyWords);
   let listadoLinks = [];
   let visible = (await page.$('[data-testid="pagination-page-next"]')) !== null;
   let i = 0;
-  while (visible && i < 10) {
+  while (visible && i < 3) {
     let nuevosLinks = await page.evaluate(() => {
       let links = [];
       let rawLinks = document.querySelectorAll(
@@ -44,8 +75,10 @@ import puppeteer from "puppeteer";
     visible = (await page.$('[data-testid="pagination-page-next"]')) !== null;
     if (visible) {
       i++;
-      await page.click('[data-testid="pagination-page-next"]');
-      await page.waitForSelector("div.jobsearch-SerpMainContent");
+      await page.evaluate(() => {
+        document.querySelector('[data-testid="pagination-page-next"]').click();
+      });
+      await page.waitForSelector("div#mosaic-jobResults");
 
       await new Promise((r) => setTimeout(r, 2000));
     }
@@ -65,9 +98,7 @@ import puppeteer from "puppeteer";
       tmp.keyWords =
         document
           .querySelector("#jobDescriptionText")
-          ?.innerText.match(
-            /(node|react|typescript|javascript|angular|next|express|nest|fullstack|backend|\.net|java|python|django|c#|spring|senior|mid|junior)/gi
-          ) ?? null;
+          ?.innerText.match(regexKeyWords) ?? null;
       return tmp;
     });
     job.salary =
@@ -95,5 +126,5 @@ import puppeteer from "puppeteer";
   console.log(informe);
   await browser.close();
 
-  //fs.writeFileSync('codigos.json', JSON.stringify(def));
+  //fs.writeFileSync("codigos.json", JSON.stringify(def));
 })();
